ea2c01203b7d68ad4799ce778903e7bc50033d9f
jmlarios <jm.larios06@gmail.com>
2025-11-27 13:24:59 +0100

    conflict fixes

--- frontend/src/main/java/com/futclub/frontend/ui/HomeView.java
+++ frontend/src/main/java/com/futclub/frontend/ui/HomeView.java
@@ -10,8 +10,6 @@ import com.futclub.model.User;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Alert;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
